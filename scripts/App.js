64685c394073b72057e2c6e53cd5d6c014fcb2a1
kamii-vii <tyhran.ioannisian@gmail.com>
2023-07-06 10:38:42 +0300

    Update App.js

--- scripts/App.js
+++ scripts/App.js
@@ -8,19 +8,29 @@ document.addEventListener('mousemove', e =>{
 })
 })
 
-window.addEventListener('devicemotion', function(event) {
-    var accelerationX = event.accelerationIncludingGravity.x;
-    var accelerationY = event.accelerationIncludingGravity.y;
+window.addEventListener('devicemotion', e => {
+    Object.assign(document.documentElement, {
+        style: `
+        --move-x-ac: ${(e.clientX - window.innerWidth / 2) * -.005}deg;
+        --move-y-ac: ${(e.clientY - window.innerHeight / 2) * .005}deg;
+
+        `
+})
+})
+
+
+//     var accelerationX = event.accelerationIncludingGravity.x;
+//     var accelerationY = event.accelerationIncludingGravity.y;
   
-    // Виклик функції для зміни об'єкту на основі прискорення
-    transformObject(accelerationX, accelerationY);
-  });
+//     // Виклик функції для зміни об'єкту на основі прискорення
+//     transformObject(accelerationX, accelerationY);
+//   });
   
-  // Функція, яка змінює об'єкт на основі прискорення
-  function transformObject(accelerationX, accelerationY) {
-    // Ваш код для зміни об'єкту на основі значень прискорення
-    // Наприклад, встановлення стилів CSS об'єкта на основі прискорення
-    var object = document.getElementsByClassName('layers__container');
-    object.style.setProperty('--move-x-ac', (accelerationX * -0.005) + 'deg');
-    object.style.setProperty('--move-y-ac', (accelerationY * 0.005) + 'deg');
-  }
+//   // Функція, яка змінює об'єкт на основі прискорення
+//   function transformObject(accelerationX, accelerationY) {
+//     // Ваш код для зміни об'єкту на основі значень прискорення
+//     // Наприклад, встановлення стилів CSS об'єкта на основі прискорення
+//     var object = document.getElementsByClassName('layers__container');
+//     object.style.setProperty('--move-x-ac', (accelerationX * -0.005) + 'deg');
+//     object.style.setProperty('--move-y-ac', (accelerationY * 0.005) + 'deg');
+//   }
